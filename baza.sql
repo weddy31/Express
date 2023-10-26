c420f7fbcd8dc86b5c04ae738bc2806adf211a81
weddy31 <99147620+weddy31@users.noreply.github.com>
2023-10-26 22:16:47 +0200

    Create baza.sql

---
+++ baza.sql
@@ -0,0 +1,33 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 26 Paź 2023, 22:15
+-- Wersja serwera: 10.4.14-MariaDB
+-- Wersja PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `123`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `dane`
+--
+
+CREATE TABLE `dane` (
+  `id` int(11) NOT NULL,
+  `kolumna_123321` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
